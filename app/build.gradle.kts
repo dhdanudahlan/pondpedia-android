3f9694e2fb5afb880da1ef9a63909c1bbc951053
KylixEza <kylixeza.dev@gmail.com>
2024-02-07 20:54:05 +0700

    build(app): bump release version code to 9 and version name to 1.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.pondpedia.android.pondpedia"
         minSdk = 28
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.2.6"
+        versionCode = 9
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
